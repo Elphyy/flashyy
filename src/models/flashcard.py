0342dc580d79c76d6fa78e7870c3ca7c841bd889
Lucas Signorelli <signorellilucas7@gmail.com>
2024-09-14 19:12:14 +0200

    add: update_freshness function
    I added the function to update the freshness of a card. It is based on the date the person last seen the card. Need to add a played function to update the date.

--- src/models/flashcard.py
+++ src/models/flashcard.py
@@ -1,6 +1,7 @@
 # This file implement the logic behind the flashcard system.
 # A flashcard is a card bearing a question and an answer.
 import json
+from datetime import datetime, timedelta
 from uuid import uuid4
 
 
@@ -11,12 +12,14 @@ class Flashcard:
     answer: str
     elo: int
     freshness: int
+    created_at: datetime.now().date()
+    last_studied_at: datetime.now().date()
 
     def __init__(self, question :str, answer :str):
         """
         Initialize a Flashcard object.
         The Elo rating, initially set to 50, represents the question's difficulty; it adjusts based on the user's performance when answering the question (cf. update_elo() function).
-        The freshness value, initially set to 1, indicates how recently the card was answered; it influences the question's Elo rating and is updated daily when the user responds to the card (cf. update_freshness() function)
+        The freshness value, initially set to 0, indicates how recently the card was answered; it influences the question's Elo rating and is updated daily when the user responds to the card (cf. update_freshness() function)
         :param question: string representing the question asked.
         :param answer: string representing the answer wanted.
         """
@@ -24,13 +27,21 @@ class Flashcard:
         self.question = question
         self.answer = answer
         self.elo = 50
-        self.freshness = 1
+        self.freshness = 0
+        self.created_at = datetime.now().date()
+        self.last_studied_at = datetime.now().date()
 
     def update_elo(self) -> int:
         return 0
 
-    def update_freshness(self) -> int:
-        return 0
+    def update_freshness(self) -> None:
+        current_date = datetime.now().date()
+        days_difference = (current_date - self.created_at).days
+        if days_difference > 0 and current_date == self.last_studied_at :
+            self.freshness += days_difference
+
+    def update_last_studied(self) -> None:
+        self.last_studied_at = datetime.now().date()
 
     def jsonify(self) -> str:
         """
@@ -42,5 +53,7 @@ class Flashcard:
             'question': self.question,
             'answer': self.answer,
             'elo': self.elo,
-            'freshness': self.freshness
-        }, indent=2)
+            'freshness': self.freshness,
+            'created_at': self.created_at,
+            'last_studied_at': self.last_studied_at
+        }, indent=2, default=str)
